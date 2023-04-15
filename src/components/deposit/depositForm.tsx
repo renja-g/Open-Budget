e6bfba7005a6eeafe32e29a44eb9316419084351
Renja <renja.grotemeyer@gmail.com>
2023-04-16 00:58:12 +0200

    Worked on the depositForm

--- src/components/deposit/depositForm.tsx
+++ src/components/deposit/depositForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Button } from '~/components/ui/button';
 import { Input } from '~/components/ui/input';
 import { Label } from '~/components/ui/label';
@@ -19,15 +20,64 @@ import {
 } from '~/components/ui/dialog';
 import { type DepositType } from '~/types';
 
-interface DepositFormProps {
+export interface DepositFormProps {
   name?: string;
   description?: string;
-  balance?: string;
+  balance?: number;
   depositType?: DepositType;
+  onSubmit?: (formData: DepositFormProps) => void;
 }
 
 export function DepositForm(props: DepositFormProps) {
-  const { name, description, balance, depositType } = props;
+  const [name, setName] = useState(props.name || '');
+  const [description, setDescription] = useState(props.description || '');
+  const [balance, setBalance] = useState(props.balance || 0);
+  const [depositType, setDepositType] = useState(
+    props.depositType || 'CURRENT'
+  );
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    props.onSubmit?.({
+      name,
+      description,
+      balance,
+      depositType,
+    });
+    resetForm();
+  };
+
+  const resetForm = () => {
+    setName('');
+    setDescription('');
+    setBalance(0);
+    setDepositType('CURRENT');
+  };
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const handleDescriptionChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setDescription(event.target.value);
+  };
+
+  const handleBalanceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value.trim();
+    const value =
+      inputValue !== '' && !isNaN(Number(inputValue))
+        ? parseInt(inputValue, 10)
+        : 0;
+    setBalance(value);
+  };
+
+  const handleDepositTypeChange = (
+    value: string
+    ) => {
+    setDepositType(value as DepositType);
+  };
 
   return (
     <Dialog>
@@ -39,60 +89,67 @@ export function DepositForm(props: DepositFormProps) {
           <DialogTitle>Deposit</DialogTitle>
           <DialogDescription>Create a new deposit.</DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
-              id="name"
-              placeholder="Deposit name"
-              className="col-span-3"
-              value={name}
-            />
+        <form onSubmit={handleSubmit}>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <Input
+                id="name"
+                placeholder="Deposit name"
+                className="col-span-3"
+                value={name}
+                onChange={handleNameChange}
+                required
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="description" className="text-right">
+                Description
+              </Label>
+              <Input
+                id="description"
+                placeholder="Deposit description"
+                className="col-span-3"
+                value={description}
+                onChange={handleDescriptionChange}
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="balance" className="text-right">
+                Balance
+              </Label>
+              <Input
+                id="balance"
+                placeholder="Deposit balance"
+                className="col-span-3"
+                value={balance}
+                onChange={handleBalanceChange}
+                required
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="depositType" className="text-right">
+                Deposit Type
+              </Label>
+              <Select value={depositType} onValueChange={handleDepositTypeChange} required>
+                <SelectTrigger className="col-span-3">
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="CURRENT">Current</SelectItem>
+                  <SelectItem value="CASH">Cash</SelectItem>
+                  <SelectItem value="CREDIT">Credit</SelectItem>
+                  <SelectItem value="OTHER">Other</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="description" className="text-right">
-              Description
-            </Label>
-            <Input
-              id="description"
-              placeholder="Deposit description"
-              className="col-span-3"
-              value={description}
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="balance" className="text-right">
-              Balance
-            </Label>
-            <Input
-              id="balance"
-              placeholder="Deposit balance"
-              className="col-span-3"
-              value={balance}
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="depositType" className="text-right">
-              Deposit Type
-            </Label>
-            <Select value={depositType}>
-              <SelectTrigger className="col-span-3">
-                <SelectValue />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="CURRENT">Current</SelectItem>
-                <SelectItem value="CASH">Cash</SelectItem>
-                <SelectItem value="CREDIT">Credit</SelectItem>
-                <SelectItem value="OTHER">Other</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-        </div>
         <DialogFooter>
           <Button type="submit">Create</Button>
         </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
--- src/pages/deposit/index.tsx
+++ src/pages/deposit/index.tsx
@@ -1,13 +1,18 @@
-import { DepositForm } from '~/components/deposit/depositForm';
+import { DepositForm, type DepositFormProps } from '~/components/deposit/depositForm';
 
 const Deposit = () => {
+  const handleSubmit = (formData: DepositFormProps) => {
+    // Add your logic here for handling form submission
+    console.log('Form data:', formData);
+  };
+
   return (
     <div>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Deposit
       </h1>
       <div className="[&:not(:first-child)]:mt-6">
-        <DepositForm />
+        <DepositForm onSubmit={handleSubmit} />
       </div>
     </div>
   );
