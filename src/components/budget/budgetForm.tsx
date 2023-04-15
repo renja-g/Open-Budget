87cd3c32a9efd76aacfb9d538327a3377d1938bf
Renja <renja.grotemeyer@gmail.com>
2023-04-15 18:03:07 +0200

    Played with onSubmit

--- src/components/budget/budgetForm.tsx
+++ src/components/budget/budgetForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Button } from '~/components/ui/button';
 import { Input } from '~/components/ui/input';
 import { Label } from '~/components/ui/label';
@@ -11,14 +12,48 @@ import {
   DialogTrigger,
 } from '~/components/ui/dialog';
 
-interface BudgetFormProps {
+export interface BudgetFormProps {
   name?: string;
   description?: string;
   amount?: number;
+  onSubmit?: (formData: BudgetFormProps) => void;
 }
 
 export function BudgetForm(props: BudgetFormProps) {
-  const { name, description, amount } = props;
+  const [name, setName] = useState(props.name || '');
+  const [description, setDescription] = useState(props.description || '');
+  const [amount, setAmount] = useState(props.amount || 0);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    props.onSubmit?.({
+      name,
+      description,
+      amount,
+    });
+    resetForm();
+  };
+
+  const resetForm = () => {
+    setName('');
+    setDescription('');
+    setAmount(0);
+  };
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const handleDescriptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDescription(event.target.value);
+  };
+
+  const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value.trim();
+    const value = inputValue !== '' && !isNaN(Number(inputValue)) ? parseInt(inputValue, 10) : 0;
+    setAmount(value);
+  };
+  
 
   return (
     <Dialog>
@@ -30,44 +65,49 @@ export function BudgetForm(props: BudgetFormProps) {
           <DialogTitle>Budget</DialogTitle>
           <DialogDescription>Create a new budget.</DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
-              id="name"
-              placeholder="Budget name"
-              className="col-span-3"
-              value={name}
-            />
+        <form onSubmit={handleSubmit}>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <Input
+                id="name"
+                placeholder="Budget name"
+                className="col-span-3"
+                value={name}
+                onChange={handleNameChange}
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="description" className="text-right">
+                Description
+              </Label>
+              <Input
+                id="description"
+                placeholder="Budget description"
+                className="col-span-3"
+                value={description}
+                onChange={handleDescriptionChange}
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="amount" className="text-right">
+                Amount
+              </Label>
+              <Input
+                id="amount"
+                placeholder="Budget amount"
+                className="col-span-3"
+                value={amount}
+                onChange={handleAmountChange}
+              />
+            </div>
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="description" className="text-right">
-              Description
-            </Label>
-            <Input
-              id="description"
-              placeholder="Budget description"
-              className="col-span-3"
-              value={description}
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="amount" className="text-right">
-              Amount
-            </Label>
-            <Input
-              id="amount"
-              placeholder="Budget amount"
-              className="col-span-3"
-              value={amount}
-            />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Create</Button>
-        </DialogFooter>
+          <DialogFooter>
+            <Button type="submit">Create</Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
--- src/pages/testing/budget/index.tsx
+++ src/pages/testing/budget/index.tsx
@@ -1,13 +1,20 @@
-import { BudgetForm } from "~/components/budget/budgetForm";
+import { BudgetForm, type BudgetFormProps } from "~/components/budget/budgetForm";
+
+
 
 const Budget = () => {
+  const handleSubmit = (formData: BudgetFormProps) => {
+    // Add your logic here for handling form submission
+    console.log('Form data:', formData);
+  };
+
   return (
     <div>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
       Budget
       </h1>
       <div className="[&:not(:first-child)]:mt-6">
-        <BudgetForm />
+      <BudgetForm onSubmit={handleSubmit} />
       </div>
     </div>
   );
